15e75d4342ed4209deb3e31a3d989fd01fdca8cc
Astrogildo Correia <108525041+astrocorreia@users.noreply.github.com>
2022-07-27 18:17:16 +0100

    Testando  Condições

--- aula010/aula011/aula12/ex011.js
+++ aula010/aula011/aula12/ex011.js
@@ -1,9 +1,12 @@
-var idade = 22
-if (idade <18) {
-    console.log('Menor de Idade')
+var idade = 65 
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Não Vota')
 
+}
+else if (idade < 18 || idade > 65) {
+    console.log('Voto opcional')
 }
 else {
-    console.log('Maior de Idade')
+    console.log('Voto obrigatório')
 }
-
