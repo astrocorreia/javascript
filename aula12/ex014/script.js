0ebf6b35dc71b1edf314d72b907ef18bc493614e
Astrogildo Correia <108525041+astrocorreia@users.noreply.github.com>
2022-07-29 23:28:11 +0100

    Teste Site 1

--- aula12/ex014/script.js
+++ aula12/ex014/script.js
@@ -1,9 +1,11 @@
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date ()
-//var hora = data.getHours()
-var hora = 12
-msg.innerHTML = `Agora são ${hora}horas.`
+
+var hora = data.getHours()
+//var hora = 12
+
+msg.innerHTML = `Agora são ${hora} horas.`
  
 if (hora >= 0 && hora < 12) {
     //BOM DIA!
