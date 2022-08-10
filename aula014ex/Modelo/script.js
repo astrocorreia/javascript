7d33b393ab02593fc21f10fa65ea881c3fd5e7a8
Astrogildo Correia <108525041+astrocorreia@users.noreply.github.com>
2022-08-10 16:27:15 +0100

    Exercício Tabuada

--- aula014ex/Modelo/script.js
+++ aula014ex/Modelo/script.js
@@ -1,32 +1,21 @@
-function contar () {
+function tabuada () {
     
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-    if (ini.value.length == 0 ||fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
-        res.innerHTML = 'IMPOSSÍVEL CONTAR!'
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+        
     } else {
-        res.innerHTML = 'Contando: <br>'
-        let i = Number (ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
+        let n = Number (num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
 
-        if (i < f) {
-            //Contagem Crescente
-         for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} \u{1F449}`
-         }
-         
-         } else {
-            //Contagem Decrescente
-            for(let c = i; c >= f; c -= p) {
-                res.innerHTML += `${c} \u{1F449}`
-         }    
-    }
-        res.innerHTML += `\u{1F3C1}` 
-   
-    }
+    } 
 }
